a16a399644557e785d0556251b73c79dbba8771b
lygjwy <lygjwy@qq.com>
2022-07-09 21:39:12 +0800

    fix bug

--- scripts/train-cifar10.sh
+++ scripts/train-cifar10.sh
@@ -1,7 +1,7 @@
 # Train CIFAR10 CLF
 
 # random sampling
-python train_rand_resa.py --id cifar10 --ood tiny_images --lr 0.1 --epochs 100 --gpu_idx 0
+python train_random_resa.py --id cifar10 --ood tiny_images --lr 0.1 --epochs 100 --gpu_idx 0
 
 # weighted resampling
 for w in conf dens; do
